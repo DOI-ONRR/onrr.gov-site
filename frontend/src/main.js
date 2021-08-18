3999cbf7d11253de7d95aadaaf87246465c5f538
Jerome <jerome.louis@onrr.gov>
2021-08-18 11:42:57 -0600

    opt for node-fetch, IE errors

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import router from './router'
 import VueApollo from 'vue-apollo'
 import ApolloClient from 'apollo-boost'
 import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory'
-import fetch from 'unfetch'
+import fetch from 'node-fetch'
 import vuetify from './plugins/vuetify'
 import App from './App.vue'
 import introspectionQueryResultData from './json/possibleTypes.json'
@@ -20,19 +20,19 @@ const cache = new InMemoryCache({
 // console.log('cache yo ------> ', cache)
 
 const apolloClient = new ApolloClient({
-  fetchOptions: { fetch },
+  fetch: fetch,
   uri: `${ process.env.VUE_APP_API_URL }/graphql`,
   cache
 })
 
 const clientA = new ApolloClient({
-  fetchOptions: { fetch },
+  fetch: fetch,
   uri: `${ process.env.VUE_APP_API_URL }/graphql/system`,
   cache
 })
 
 const clientB = new ApolloClient({
-  fetchOptions: { fetch },
+  fetch: fetch,
   uri: `${ process.env.VUE_APP_NRRD_API_URL }/v1/graphql`,
   cache
 })
