9a8df8f227c1a04bda06528e9e51abef3073a09e
Mojo Nichols <mojo.nichols@onrr.gov>
2021-09-07 08:40:14 -0600

    got db separated from directus, made directus independ of secret variables stored in repo

---
+++ database/ddl/trigger_link_upsert.sql
@@ -0,0 +1,20 @@
+drop function function_page_link_upsert CASCADE;
+create function function_page_link_upsert() 
+RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS $$ 
+BEGIN
+IF NEW.status = 'published' THEN
+insert into links(url, label, category,status, date_created) values (concat('/',NEW.slug), NEW.title, 'Pages', 'published',now() ); 
+END IF;
+RETURN NEW;
+END;
+$$
+;
+
+
+
+CREATE TRIGGER trigger_page_links AFTER INSERT OR UPDATE ON PAGES
+FOR EACH ROW
+EXECUTE PROCEDURE  function_page_link_upsert()
+;
