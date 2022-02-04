b2ccac45c0fa68b479e13c6ad90a5bb4aea44a16
jromey <jromey0000@gmail.com>
2022-02-04 16:35:40 -0700

    reporter letters, topic filters, topic query params

--- database/src/ELT/reporter_letters.sql
+++ database/src/ELT/reporter_letters.sql
@@ -2,12 +2,12 @@ CREATE SEQUENCE reporter_letters_sequence
   start 1
   increment 1;
 
-\copy reporter_letters (status,sort,user_created,date_created,user_updated,date_updated,title,date,file,link,accessible_file) FROM './static/csv/reporter_letters.csv' WITH  DELIMITER ',' CSV HEADER;
+\copy reporter_letters (status,sort,user_created,date_created,user_updated,date_updated,title,date,file,link,accessible_file,topics) FROM './static/csv/reporter_letters.csv' WITH  DELIMITER ',' CSV HEADER;
 
 \echo 'Insert into reporter_letters'
 INSERT INTO reporter_letters 
-  (id,status,sort,user_created,date_created,user_updated,date_updated,title,date,file,link,accessible_file)
+  (id,status,sort,user_created,date_created,user_updated,date_updated,title,date,file,link,accessible_file,topics)
 VALUES 
-  (nextval('reporter_letters_sequence'),status,sort,user_created,date_created,user_updated,date_updated,title,date,file,link,accessible_file);
+  (nextval('reporter_letters_sequence'),status,sort,user_created,date_created,user_updated,date_updated,title,date,file,link,accessible_file,topics);
 
 select * from reporter_letters;
