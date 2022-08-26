35ed166dd7b30c20f020b2c09994b23a3c3a0159
Mojo Nichols <mojo.nichols@onrr.gov>
2022-08-26 08:21:13 -0600

    bug fix circle ci script

--- frontend/dist/js/EventsCollection-legacy.268e2872.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkonrr_frontend_app_vue"]=self["webpackChunkonrr_frontend_app_vue"]||[]).push([[495],{9333:function(e,n,t){t.r(n),t.d(n,{default:function(){return a}});var r=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("h2",[e._v("Hello, from the events collection!")])},l=[],o={name:"EventsCollection"},u=o,c=t(1001),f=(0,c.Z)(u,r,l,!1,null,"f234a934",null),a=f.exports}}]);
-//# sourceMappingURL=EventsCollection-legacy.268e2872.js.map
