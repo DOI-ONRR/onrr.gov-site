ad8cda5260a0072371a8308b52c71fd833f1e809
Maroyafaied <40175688+Maroyafaied@users.noreply.github.com>
2022-09-08 18:09:16 -0600

    icons fix

--- frontend/src/mixins/iconMixin.js
+++ frontend/src/mixins/iconMixin.js
@@ -1,36 +1,69 @@
-export const iconMixin = {
-  methods: {
-   fileIcon(fileType) {
-      let type 
-      switch (fileType) {
-        case 'application/pdf':
-        case 'pdf':
-          type = 'mdi-file-pdf-box'
-          break;
-        case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-          type = 'mdi-file-word-box'
-          break;
-        case 'xls':
-        case 'xlsx':
-        case 'vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-        case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-          type = 'mdi-file-excel-box'
-          break;
-        case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
-       case 'application/vnd.ms-powerpoint':
-      case 'application/vnd.ms-powerpoint.presentation.macroEnabled.12':
-          type = 'mdi-file-powerpoint-box'
-          return 'x-office-presentation';
-           break;
-          case 'plain':
-          type = 'mdi-text-box'
-          break
-        default:
-          type = undefined
-          break;
-      }
+Code
+function file_icon_map($mime_type) {
+  switch ($mime_type) {
 
-      return type;
-    },
+    // Word document types.
+    case 'application/msword':
+    case 'application/vnd.ms-word.document.macroEnabled.12':
+    case 'application/vnd.oasis.opendocument.text':
+    case 'application/vnd.oasis.opendocument.text-template':
+    case 'application/vnd.oasis.opendocument.text-master':
+    case 'application/vnd.oasis.opendocument.text-web':
+    case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+    case 'application/vnd.stardivision.writer':
+    case 'application/vnd.sun.xml.writer':
+    case 'application/vnd.sun.xml.writer.template':
+    case 'application/vnd.sun.xml.writer.global':
+    case 'application/vnd.wordperfect':
+    case 'application/x-abiword':
+    case 'application/x-applix-word':
+    case 'application/x-kword':
+    case 'application/x-kword-crypt':
+      return 'x-office-document';
+
+    // Spreadsheet document types.
+    case 'application/vnd.ms-excel':
+    case 'application/vnd.ms-excel.sheet.macroEnabled.12':
+    case 'application/vnd.oasis.opendocument.spreadsheet':
+    case 'application/vnd.oasis.opendocument.spreadsheet-template':
+    case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+    case 'application/vnd.stardivision.calc':
+    case 'application/vnd.sun.xml.calc':
+    case 'application/vnd.sun.xml.calc.template':
+    case 'application/vnd.lotus-1-2-3':
+    case 'application/x-applix-spreadsheet':
+    case 'application/x-gnumeric':
+    case 'application/x-kspread':
+    case 'application/x-kspread-crypt':
+      return 'x-office-spreadsheet';
+
+    // Presentation document types.
+    case 'application/vnd.ms-powerpoint':
+    case 'application/vnd.ms-powerpoint.presentation.macroEnabled.12':
+    case 'application/vnd.oasis.opendocument.presentation':
+    case 'application/vnd.oasis.opendocument.presentation-template':
+    case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+    case 'application/vnd.stardivision.impress':
+    case 'application/vnd.sun.xml.impress':
+    case 'application/vnd.sun.xml.impress.template':
+    case 'application/x-kpresenter':
+      return 'x-office-presentation';
+
+
+    // HTML aliases.
+    case 'application/xhtml+xml':
+      return 'text-html';
+
+
+
+    // Acrobat types
+    case 'application/pdf':
+    case 'application/x-pdf':
+    case 'applications/vnd.pdf':
+    case 'text/pdf':
+    case 'text/x-pdf':
+      return 'application-pdf';
+    default:
+      return FALSE;
   }
 }
