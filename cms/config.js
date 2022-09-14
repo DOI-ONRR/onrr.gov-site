1e62b69ba91734091a479d62d39e24f675d1cb5a
Mojo Nichols <mojo.nichols@onrr.gov>
2022-09-14 11:46:01 -0600

    compiled case with ppt

--- cms/config.js
+++ cms/config.js
@@ -26,22 +26,23 @@ module.exports = function(env) {
     STORAGE_AWS_ENDPOINT: vcap_services['s3'][0].credentials.endpoint,
     STORAGE_AWS_BUCKET: vcap_services['s3'][0].credentials.bucket,
     STORAGE_AWS_REGION: vcap_services['s3'][0].credentials.region,
-    NODE_ENV: evelopment    
+    
     // CACHE_ENABLED: true,
     // CACHE_STORE: "redis",
     // CACHE_REDIS: "redis://cache:6379",
     CACHE_AUTO_PURGE: true,
-    ADMIN_EMAIL: `${ vcap_application.organization_name }@onrr.gov`,
-    ADMIN_PASSWORD: vcap_application.organization_id,
     
+    ADMIN_EMAIL: `${ vcap_application.rganization_name }@onrr.gov`,
+    ADMIN_PASSWORD: vcap_application.organization_id,
+    NODE_ENV: development,    
     EMAIL_SENDMAIL_NEW_LINE: "unix",
     EMAIL_SENDMAIL_PATH: "/usr/sbin/sendmail",
     EMAIL_FROM: "no-reply@directus.io", 
     EMAIL_TRANSPORT: "sendmail",
     GITHUB_TOKEN: env.GITHUB_TOKEN,
-    MAX_RELATIONAL_DEPTH: 200,
+    MAX_RELATIONAL_DEPTH: 2000,
     // https://github.com/directus/directus/releases
     CORS_ENABLED: true,
-    CORS_ORIGIN: 'array:https://prod-onrr-cms.app.cloud.gov,https://prod-onrr-frontend.app.cloud.gov,https://beta.onrr.gov,http://0.0.0.0:8055'
+    CORS_ORIGIN: 'array:https://dev-onrr-cms.app.cloud.gov,https://dev-onrr-frontend.app.cloud.gov,http://0.0.0.0:8055'
   }
 };
