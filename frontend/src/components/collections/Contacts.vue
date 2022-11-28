0a3d452262106d9e56e4bb7b937668d2ac50bfe3
Mohan Reddy <mohansaikumar.reddy@onrr.gov>
2022-11-28 13:13:48 -0600

    update headers-1

--- frontend/src/components/collections/Contacts.vue
+++ frontend/src/components/collections/Contacts.vue
@@ -53,8 +53,9 @@
               <span v-if="item.operatorNumber !== null">(Operator #: {{ item.operatorNumber }})</span>
             </span>
           </h5>
-          <div v-if="headerChange(item) > 1">
-          <h4 class="collection-category pa-3 mb-3" style="font-size:large;">
+        </div>
+        <div v-if="headerChange(item) === 1">
+          <h3 class="collection-category pa-3 mb-3">
             <span v-if="!searchResults">
               {{ item.header }}
               <span v-if="item.agency !== null">({{ item.agency }})</span>
@@ -65,8 +66,7 @@
               <span v-if="item.agency !== null">({{ item.agency }})</span>
               <span v-if="item.operatorNumber !== null">(Operator #: {{ item.operatorNumber }})</span>
             </span>
-          </h4>
-        </div>
+          </h3>
         </div>
           <div v-if="headerChange(item) === 1">
           <h3 class="collection-category pa-3 mb-3">
