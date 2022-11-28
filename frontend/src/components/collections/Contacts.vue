c4450d7508abb1e8c304d713414c3679fde04bfe
Mohan Reddy <mohansaikumar.reddy@onrr.gov>
2022-11-27 23:04:25 -0600

    update build

--- frontend/src/components/collections/Contacts.vue
+++ frontend/src/components/collections/Contacts.vue
@@ -41,7 +41,6 @@
       <v-fade-transition group hide-on-leave leave-absolute origin="top left">
         <div v-for="(item, i) in visibleItems" :key="i" class="mb-5">
           <div v-if="headerChange(item) > 0">
-            <component :is="'h'+headerChange(item)">Sample Header</component>
           <h3 class="collection-category pa-3 mb-3">
             <span v-if="!searchResults">
               {{ item.header }}
