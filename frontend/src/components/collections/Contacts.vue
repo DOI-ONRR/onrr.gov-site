04a6fef7f7b2cb3cda360673002a6b247d96af15
Mohan Reddy <mohansaikumar.reddy@onrr.gov>
2022-11-28 20:38:00 -0600

    h5 change

--- frontend/src/components/collections/Contacts.vue
+++ frontend/src/components/collections/Contacts.vue
@@ -40,6 +40,7 @@
     <div v-if="visibleItems.length > 0 && showResults">
       <v-fade-transition group hide-on-leave leave-absolute origin="top left">
         <div v-for="(item, i) in visibleItems" :key="i" class="mb-5">
+          <span>Mohan checking</span>
           <component :is="headerTagValue">checking Value</component>
           <div v-if="headerChange(item) > 2">
           <h5 class="collection-category pa-3 mb-3" style="font-size:large;">
