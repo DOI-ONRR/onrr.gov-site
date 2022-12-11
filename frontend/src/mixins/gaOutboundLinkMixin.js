36aafdbf55486a36412c56ef8e568c74540a5121
Jeff Schwartz <jschwartz@greensomes.com>
2022-12-11 14:39:35 -0500

    dev: Adds nextTick

--- frontend/src/mixins/gaOutboundLinkMixin.js
+++ frontend/src/mixins/gaOutboundLinkMixin.js
@@ -1,40 +1,42 @@
 export const gaOutboundLinkMixin = {
     mounted: function() {
-        const links = this.$el.querySelectorAll('a');
+        this.$nextTick(function() {
+            const links = this.$el.querySelectorAll('a');
 
-        console.log(links);
+            console.log(links);
 
-        if (links.length == 0)
-            return;
+            if (links.length == 0)
+                return;
 
-        const re = /^((http[s]?):\/)?\/?([^:/\s]+)((\/\w+)*\/)([\w\-.]+[^#?\s]+)(.*)?(#[\w-]+)?$/;
-        const result = re.exec(window.location.href);
+            const re = /^((http[s]?):\/)?\/?([^:/\s]+)((\/\w+)*\/)([\w\-.]+[^#?\s]+)(.*)?(#[\w-]+)?$/;
+            const result = re.exec(window.location.href);
 
-        if (result == null)
-            return;
+            if (result == null)
+                return;
 
-        const hostname = result[3];
+            const hostname = result[3];
 
-        for (var i = 0; i < links.length; i++) {
-            var link = links[i];
+            for (var i = 0; i < links.length; i++) {
+                var link = links[i];
 
-            if (link.hostname == hostname)
-                continue;
-            
-            const linkResult = re.exec(link.href);
+                if (link.hostname == hostname)
+                    continue;
+                
+                const linkResult = re.exec(link.href);
 
-            var eventLabel = '/';
+                var eventLabel = '/';
 
-            if (linkResult != null && linkResult.length > 6)
-                eventLabel += linkResult[6];
+                if (linkResult != null && linkResult.length > 6)
+                    eventLabel += linkResult[6];
 
-            link.addEventListener('click', () => {
-                this.$ga.event({
-                    eventCategory: 'Outbound',
-                    eventAction: link.hostname,
-                    eventLabel: eventLabel
-                });
-            })
-        }
+                link.addEventListener('click', () => {
+                    this.$ga.event({
+                        eventCategory: 'Outbound',
+                        eventAction: link.hostname,
+                        eventLabel: eventLabel
+                    });
+                })
+            }
+        });
     }
 }
