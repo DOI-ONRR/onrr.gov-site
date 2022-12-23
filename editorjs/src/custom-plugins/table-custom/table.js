cd09a108391f01857dc5771db1a37fa8b88b7a6f
Mohan Reddy <mohansaikumar.reddy@onrr.gov>
2022-12-23 10:41:10 -0600

    Adding header fixes

--- editorjs/src/custom-plugins/table-custom/table.js
+++ editorjs/src/custom-plugins/table-custom/table.js
@@ -528,7 +528,7 @@ export default class Table {
   }
 
   createTableHeading() {
-    this.textareaEle = $.make('h2', CSS.headingClass, { contentEditable: "true" });
+    this.textareaEle = $.make('span', CSS.headingClass, { contentEditable: "true" });
     this.textareaEle.innerHTML = this.data.tableHeadingText || '';
     //this.textareaEle.dataset.placeholder = this.api.i18n.t(this.data.tableHeadingText || '');
   }
@@ -812,6 +812,7 @@ export default class Table {
 
     if (withHeadings) {
       this.table.classList.add(CSS.withHeadings);
+      this.table.classList.add(`.tc-row:first-child`);
       this.addHeadingAttrToFirstRow();
     } else {
       this.table.classList.remove(CSS.withHeadings);
