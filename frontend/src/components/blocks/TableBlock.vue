4cb5ed1e00627f5a2823308625b125b75212dfef
Mohan Reddy <mohansaikumar.reddy@onrr.gov>
2022-12-29 17:08:37 -0600

    adding multiple title

--- frontend/src/components/blocks/TableBlock.vue
+++ frontend/src/components/blocks/TableBlock.vue
@@ -2,7 +2,7 @@
   <v-simple-table>
     <template v-slot:default>
       <thead>
-        <h4 v-html="tableTitleItems"></h4>
+        <span v-html="tableTitleItems"></span>
         <tr>
           <th 
             v-for="(item, index) in tableHeaderItems"
@@ -61,6 +61,13 @@ export default {
 
       return str
     },
+   
+  },
+  computed: {
+    tableHeaderItems() {
+      const hItems = this.block.data.content[0]
+      return hItems
+    },
     tableTitleItems() {
       let tvalue = this.block.data.tableHeadingText[this.headingCount];
       if(this.block.data.tableHeadingText.length > this.headingCount ){
@@ -68,12 +75,6 @@ export default {
       }
       console.log('the title :- '+tvalue);
       return tvalue;
-    }
-  },
-  computed: {
-    tableHeaderItems() {
-      const hItems = this.block.data.content[0]
-      return hItems
     },
     tableRowItems() {
       const rItems = this.block.data.content.filter((item, index) => index > 0)
