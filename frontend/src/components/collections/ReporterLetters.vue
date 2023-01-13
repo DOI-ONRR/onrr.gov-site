aaa795760eaf35fa850b7e5d93262ee5b49bbeaa
Faied <faiedm@mms.gov>
2023-01-13 08:56:34 -0700

    reporter letters column

--- frontend/src/components/collections/ReporterLetters.vue
+++ frontend/src/components/collections/ReporterLetters.vue
@@ -48,10 +48,10 @@
             </template>
             <template v-slot:[`header.title`]="{ header }">
                 <div class="text-h6 text-capitalize">{{ header.text}}</div>
-                <span v-if="header.text">"{{ header.text }}"</span>
             </template>
             <template v-slot:[`header.date`]="{ header }">
-            <div class="text-h6 text-capitalize">{{ header.text }}</div>
+            <div class="text-h6 text-capitalize">{{ header.text }}
+            </div>
             </template>
             <template v-slot:[`header.topics`]="{ header }">
                 <div class="text-h6 text-capitalize">{{ header.text }}</div>
@@ -66,7 +66,7 @@
               </div>
             </template>
             <template v-slot:[`item.date`]="{ item }">
-                {{ formatNiceDate(item.date) }}
+            <span v-if="formatNiceDate(item.date)">"{{formatNiceDate(item.date)}}"</span>
             </template>
             <template v-slot:[`item.topics`]="{ item }">
                 {{ getTopics(item.topics) }}
