c72176c82c362464cdd8c6a16630b343a9b4f466
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2023-09-26 16:06:38 -0400

    2840 prepare a shutdown bannermessage (#2847)
    * 2840-shutdown-banner: Adds banner under main nav
    
    * 2840-shutdown-banner: Tweaks styles

--- frontend/src/components/sections/ShutdownBanner.vue
+++ frontend/src/components/sections/ShutdownBanner.vue
@@ -1,6 +1,7 @@
 <template>
     <v-alert
         type="info"
+        class="shutdown-banner"
       >
       Due to the government shutdown, we are not able to update this site. We will resume work on the site when the shutdown ends.
       </v-alert>
@@ -11,3 +12,10 @@ export default {
 
 }
 </script>
+
+<style scoped>
+    .shutdown-banner {
+        margin-bottom: 0;
+        border-radius: 0 !important;
+    }
+</style>
