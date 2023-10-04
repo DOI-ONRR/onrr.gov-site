0efc894711f52c9901ed210a304e057da062e74d
Maroyafaied <40175688+Maroyafaied@users.noreply.github.com>
2023-10-04 16:35:25 -0600

    Update AppToolbar.vue
    update Denver's address

--- frontend/src/components/toolbars/AppToolbar.vue
+++ frontend/src/components/toolbars/AppToolbar.vue
@@ -39,8 +39,9 @@
             <v-toolbar-title>
               <div class="logo-content">
                 <span>U.S. Department of the Interior</span>
-                <span>Office of Natural</span>
-                <span>Resources Revenue (ONRR)</span>
+                <span>Office of Natural Resources Revenue (ONRR)</span>
+                <span>PO Box 25165</span>
+                <span>Denver, CO 80225-0165</span>
               </div>
             </v-toolbar-title>
             </router-link>
