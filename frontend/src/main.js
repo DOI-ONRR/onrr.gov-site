a74f0a0fa71dd891af851a4d5f34eb296871c21c
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2024-01-23 11:32:50 -0500

    2977: Adds GTM support

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueGtm from '@gtm-support/vue2-gtm'
 import VueMeta from 'vue-meta'
 import router from './router'
 import VueApollo from 'vue-apollo'
@@ -8,10 +9,8 @@ import vuetify from './plugins/vuetify'
 import App from './App.vue'
 import possibleTypes from './json/possibleTypes.json'
 
+const GTM_ID = process.env.ONRR_GTM_ID
 
-
-
- 
 const cache = new InMemoryCache({
   possibleTypes
 })
@@ -27,8 +26,6 @@ const clientAHttpLink = createHttpLink({
 const clientBHttpLink = createHttpLink({
   uri: `${ process.env.VUE_APP_NRRD_API_URL }/v1/graphql`,
 })
-
-// console.log('cache yo ------> ', cache)
 
 export const apolloClient = new ApolloClient({
   fetch: fetch,
@@ -63,6 +60,9 @@ Vue.config.productionTip = false
 
 Vue.use(VueApollo)
 Vue.use(VueMeta)
+Vue.use(VueGtm, {
+  id: GTM_ID
+})
 new Vue({
   vuetify,
   apolloProvider,
