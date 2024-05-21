b3cfb6cc41b5d42e3f21cb278799d9e97f8e49a9
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2024-05-21 14:06:06 -0400

    3086: Updates list rendering to handle nesting

---
+++ frontend/src/components/blocks/ListItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <span>
+        <li v-html="content"></li>
+        <component 
+            v-if="items.length > 0"
+            :is="listStyle === 'unordered' ? 'ul' : 'ol'"
+        >
+            <list-item v-for="item in items" :content="item.content" :items="item.items" :listStyle="listStyle" :key="getUuid(item)"/>
+        </component>
+    </span>
+</template>
+
+<script>
+import { v4 as uuidv4 } from 'uuid';
+
+export default {
+    name: "ListItem",
+    props: {
+        content: {
+            type: String,
+            required: true
+        },
+        items: {
+            type: Array
+        },
+        listStyle: {
+            type: String,
+            default: 'unordered'
+        }
+    },
+    methods: {
+        getUuid() {
+            return uuidv4();
+        }
+    }
+}
+</script>
