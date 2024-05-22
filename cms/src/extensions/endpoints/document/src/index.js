aa194f5bf2732bbe98729c40138621a2fda181cc
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2024-05-22 09:16:02 -0400

    3139: Fixes press-releases source

--- cms/src/extensions/endpoints/document/src/index.js
+++ cms/src/extensions/endpoints/document/src/index.js
@@ -1,23 +1,4 @@
-import fetch from 'node-fetch';
-import fs from 'fs';
-
-const streamToFile = (inputStream, filePath) => {
-  return new Promise((resolve, reject) => {
-    const writeStream = fs.createWriteStream(filePath)
-    inputStream
-      .pipe(writeStream)
-      .on('finish', resolve)
-      .on('error', reject)
-  })
-} 
-
-const getFile = (async (filePath,url) => {
-  let response = await fetch(url)
-  let promise = streamToFile(response.body, filePath);
-  return promise
-    
-});
-
+import * as fileUtils from '../../../../utils/file-utils';
 
 export default (router, { services, exceptions }) => {
 	const { ItemsService } = services;
@@ -35,7 +16,7 @@ export default (router, { services, exceptions }) => {
       const hostname = (req.hostname === 'localhost') ? 'localhost:8055' : req.hostname
       const url=req.protocol+'://'+hostname+'/assets/'+results[0].id;
       
-      await getFile(filePath,url);
+      await fileUtils.getFile(filePath,url);
       return res.sendFile(filePath)
     })
     .catch((error) => {
--- cms/src/extensions/endpoints/press-releases/src/index.js
+++ cms/src/extensions/endpoints/press-releases/src/index.js
@@ -1,24 +1,6 @@
-const fetch = require('node-fetch');
-const fs = require('fs');
+import * as fileUtils from '../../../../utils/file-utils';
 
 const targetUrl = process.env.NODE_ENV === 'production' ? 'https://dev-onrr-cms.app.cloud.gov' : 'http://localhost:8055'
-
-const streamToFile = (inputStream, filePath) => {
-  return new Promise((resolve, reject) => {
-    const writeStream = fs.createWriteStream(filePath)
-    inputStream
-      .pipe(writeStream)
-      .on('finish', resolve)
-      .on('error', reject)
-  })
-} 
-
-const getFile = (async (filePath,url) => {
-  let response = await fetch(url)
-  let promise = streamToFile(response.body, filePath);
-  return promise
-});
-
 
 export default (router, { services, exceptions }) => {
 	const { ItemsService } = services;
@@ -35,7 +17,7 @@ export default (router, { services, exceptions }) => {
         const filePath = `/tmp/${ file }`;
         const url = `/assets/${ results[0].id }`;
         
-        await getFile(filePath, `${ targetUrl }${ url }`);
+        await fileUtils.getFile(filePath, `${ targetUrl }${ url }`);
         return res.sendFile(filePath)
       })
 			.catch((error) => {
