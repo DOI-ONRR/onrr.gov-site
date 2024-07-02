7a88bf30e362700498344b2d78bb69cd852f4b3e
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2024-07-02 12:27:02 -0400

    3079: Updates component markup

--- frontend/src/components/collections/PressReleases.vue
+++ frontend/src/components/collections/PressReleases.vue
@@ -9,11 +9,8 @@
           class="pa-0"
           target="_blank"
         >
-          <v-list-item-icon class="mr-1">
-            <v-icon color="secondary">mdi-file-pdf-box</v-icon>
-          </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" class="secondary--text text-body-1 text-wrap text-decoration-underline"></v-list-item-title>
+            <v-list-item-title v-text="item.title" class="secondary--text text-body-1 text-wrap text-decoration-underline onrr-link-pdf"></v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
