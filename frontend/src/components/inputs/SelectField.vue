a31f9c8064ac427487b8155b3c7013118f9bee9f
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2024-07-16 17:18:34 -0500

    2706: Applies Vuetify workaround

--- frontend/src/components/inputs/SelectField.vue
+++ frontend/src/components/inputs/SelectField.vue
@@ -38,6 +38,22 @@ export default {
     },
     created() {
         this.$emit('fields', this.field)
+    },
+    mounted: function() {
+        this.$nextTick(function() {
+            // Vuetify is adding readonly="readonly" without specifiying readonly.
+            // The code below is a fix so that ANDI doesn't flag readonly and aria-readonly
+            // mismatches as violations.
+            const el = this.$refs[this.field.ref].$el
+            if (!el) return;
+            const input = el.querySelector('input')
+            if (input.hasAttribute('readonly')) {
+                input.removeAttribute('readonly')
+            }
+            if (input.hasAttribute('aria-readonly')) {
+                input.removeAttribute('aria-readonly')
+            }
+        })
     }
 
 }
