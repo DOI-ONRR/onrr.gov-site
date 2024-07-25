4ffdce86336cb3d9d690abe59c337a62faa815ee
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2024-07-25 16:33:12 -0400

    3231: Improves nested list rendering

---
+++ frontend/src/components/blocks/NestedList.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul>
+        <li v-for="item in items" :key="getKey(item)">
+            <div v-html="item.content"></div>
+            <NestedList v-if="item.items" :items="item.items" :listType="listType"/>
+        </li>
+    </ul>
+</template>
+
+<script>
+import { v4 as uuidv4 } from 'uuid';
+
+export default {
+    name: 'NestedList',
+    props: {
+        items: {
+            type: Array,
+            required: true
+        },
+        listType: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        getKey() {
+            return uuidv4();
+        }
+    }
+}
+</script>
