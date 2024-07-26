3d685af992aaff3d2f435a8590fe9e01d052385a
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2024-07-26 07:52:55 -0400

    3207: Updates ordered list formatting

--- frontend/src/components/blocks/NestedList.vue
+++ frontend/src/components/blocks/NestedList.vue
@@ -1,10 +1,10 @@
 <template>
-    <ul>
+    <component :is="listType === 'unordered' ? 'ul' : 'ol'">
         <li v-for="item in items" :key="getKey(item)">
             <div v-html="item.content"></div>
-            <NestedList v-if="item.items" :items="item.items" :listType="listType"/>
+            <NestedList v-if="item.items" :items="item.items" :listType="listType" />
         </li>
-    </ul>
+    </component>
 </template>
 
 <script>
@@ -29,3 +29,52 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+ol {
+  counter-reset: item;
+  list-style: none;
+}
+
+ol > li {
+  counter-increment: item;
+  position: relative;
+}
+
+ol > li:before {
+  content: counters(item, '.') '. ';
+  position: absolute;
+  left: -1em;
+}
+
+ol ol {
+  counter-reset: subitem;
+}
+
+ol ol > li {
+  counter-increment: subitem;
+  margin-left: 0.85em;
+}
+
+ol ol > li:before {
+  content: counters(item, '.') '.' counters(subitem, '.') '. ';
+  position: absolute;
+  left: -1.9em;
+}
+
+ol ol ol {
+  counter-reset: subsubitem;
+}
+
+ol ol ol > li {
+  counter-increment: subsubitem;
+  margin-left: 0.85em;
+}
+
+ol ol ol > li:before {
+  content: counters(item, '.') '.' counters(subitem, '.') '.'
+    counters(subsubitem, '.') '. ';
+  position: absolute;
+  left: -1em;
+}
+</style>
