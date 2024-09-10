c7f7b757cf3fcf56acb083153843d3f0d934db50
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2024-09-10 09:39:25 -0400

    directus-flows: Calls runCardBlocksCardContentBlocks

--- cms/src/extensions/endpoints/flows/src/services/cardBlocksFlow/cardBlocks.js
+++ cms/src/extensions/endpoints/flows/src/services/cardBlocksFlow/cardBlocks.js
@@ -1,28 +1,33 @@
 import { getCardBlocksById, createCardBlock, updateCardBlocksItem } from "../../operations/cardBlocks";
+import { runCardBlocksCardContentBlocks } from "./cardBlocksCardContentBlocks";
 import { Endpoints, UpstreamAuthToken, LocalAuthToken, CollectionTypes, ApiMessages } from "../../constants";
 import { logger, previousVersionExists, versionsDiffer } from "../../utils";
 
 export async function runCardBlocks(id) {
     try {
+        var appliedChanges = [];
         const latest = await getCardBlocksById(id, Endpoints.LOCAL, LocalAuthToken);
         const previous = await getCardBlocksById(id, Endpoints.UPSTREAM, UpstreamAuthToken);
         if (!previousVersionExists(previous)) {
             const createdId = await createCardBlock(latest, Endpoints.UPSTREAM, UpstreamAuthToken);
-            return {
+            appliedChanges.push({
                 id: createdId,
                 collection: CollectionTypes.CARD_BLOCKS,
                 message: ApiMessages.ITEM_CREATED
-            }
+            });
         }
         else if (versionsDiffer(previous, latest)) {
             const updatedId = await updateCardBlocksItem(id, latest, Endpoints.UPSTREAM, UpstreamAuthToken);
-            return {
+            appliedChanges.push({
                 id: updatedId,
                 collection: CollectionTypes.CARD_BLOCKS,
                 message: ApiMessages.ITEM_UPDATED
-            }
+            });
         }
-        return null;
+        const cardContentBlocksChanges = await runCardBlocksCardContentBlocks(id);
+        return cardContentBlocksChanges.length > 0
+            ? appliedChanges.concat(cardContentBlocksChanges)
+            : appliedChanges;
     }
     catch (error) {
         logger.error(`Error in runCardBlocks (${id}):`, error)
