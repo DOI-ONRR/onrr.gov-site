2b78e569684f2da73a4207ad721e11a984063397
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2024-09-13 08:15:58 -0400

    3304: Fixes bug in downloadFile

--- cms/src/extensions/endpoints/onrr-flows/src/operations/files/downloadFile.js
+++ cms/src/extensions/endpoints/onrr-flows/src/operations/files/downloadFile.js
@@ -1,7 +1,7 @@
 import { logger } from "../../utils";
 export async function downloadFile(fileUrl) {
     try {
-        const response = fetch(fileUrl);
+        const response = await fetch(fileUrl);
         if (!response.ok) {
             throw new Error(`Failed to download ${fileUrl}: ${response.statusText}`);
         }
