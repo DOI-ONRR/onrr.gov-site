0fb048ea3d2491466beecf342db8a3100dbc3c6c
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2024-09-30 11:56:51 -0400

    3333: Adds auth token to API calls

--- cms/src/extensions/endpoints/onrr-flows/src/operations/tabBlocks/getTabBlocksTabBlocks.js
+++ cms/src/extensions/endpoints/onrr-flows/src/operations/tabBlocks/getTabBlocksTabBlocks.js
@@ -2,9 +2,13 @@ import { tabBlocksTabBlocks } from "../../queries/tabBlocks";
 import { GraphQLClient } from "graphql-request";
 import { logger } from '../../utils/logger';
 
-export async function getTabBlocksTabBlocks(tabBlocksId, endpoint) {
+export async function getTabBlocksTabBlocks(tabBlocksId, endpoint, authToken) {
     try {
-        const client = new GraphQLClient(endpoint);
+        const client = new GraphQLClient(endpoint, {
+            headers: {
+                authorization: `Bearer ${authToken}`
+            }
+        });
         const data = await client.request(tabBlocksTabBlocks, { tabBlocksId: tabBlocksId });
         return data.tab_blocks_tab_blocks;
     } catch (error) {
