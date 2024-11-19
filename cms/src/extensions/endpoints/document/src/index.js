2c6ef7bdba8b4dc6fb250e0c9a53deebc80f7ffd
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2024-11-19 17:25:43 -0500

    3424: Fixes document endpoint

--- cms/src/extensions/endpoints/document/src/index.js
+++ cms/src/extensions/endpoints/document/src/index.js
@@ -1,27 +1,28 @@
 import * as fileUtils from '../../../../utils/file-utils';
 
-export default (router, { services, exceptions }) => {
-	const { ItemsService } = services;
-  const { ServiceUnavailableException } = exceptions;
+export default (router, context) => {
+	const { services, getSchema } = context;
+  const { FilesService } = services;
 
-  router.get('/:file', (req, res, next) => {
+  router.get('/:file', async (req, res, next) => {
 
-  const linkService = new ItemsService('directus_files', { schema: req.schema, accountability: req.accountability });
-  const file=req.params.file
-
-  linkService
-    .readByQuery({ fields: ['*'],  filter: {filename_download: {'_eq': file}}})
-    .then( async (results) => {
-      const filePath='/tmp/'+file
-      const hostname = (req.hostname === 'localhost') ? 'localhost:8055' : req.hostname
-      const url=req.protocol+'://'+hostname+'/assets/'+results[0].id;
-      
-      await fileUtils.getFile(filePath,url);
-      return res.sendFile(filePath)
-    })
-    .catch((error) => {
-      return next(new ServiceUnavailableException(error.message));
+    const filesService = new FilesService({ 
+      schema: await getSchema(), 
+      accountability: req.accountability 
     });
+    
+    const fileName = req.params.file;
+
+    const files = await filesService.readByQuery({ fields: ['*'],  filter: {filename_download: {'_eq': fileName}}});
+
+    const filePath = `/tmp/${fileName}`;
+    const hostname = (req.hostname === 'localhost') ? 'localhost:8055' : req.hostname
+    const url = `${req.protocol}://${hostname}/assets/${files[0].id}`;
+    
+    await fileUtils.getFile(filePath, url);
+    
+    return res.sendFile(filePath);
+
   });
 };
 
