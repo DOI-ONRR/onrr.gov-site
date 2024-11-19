24f1cabce3e6c8611c220ec0573b42723408c74f
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2024-11-19 10:50:21 -0500

    Refactor CircleCI configuration (#3427)
    * Fixes Touchpoints loading (#3420)
    
    * dev: Adds circleci job for restart
    
    * Revert "dev: Adds circleci job for restart"
    
    This reverts commit 6bdf71a5d5386856f32761ec4985490445790c65.
    
    * dev: Loads site feedback conditionally
    
    * dev: fixes when site feedback is renderable.
    
    * dev: Adds vuex
    
    * dev: Updates shared state to set pageLoaded to false before route change
    
    * dev: Adds watch to Page
    
    * dev: Handles tabs route changes
    
    * dev: Logging
    
    * dev: Debugging
    
    * dev: More of the same
    
    * dev: Debug from and to
    
    * dev: Conditionally resets pageLoaded
    
    * dev: Bumps docker image
    
    * dev: Router cleanup
    
    * 3421: Refactors configuration
    
    * 3421: Removes yq install
    
    * 3421: Adds executable perms
    
    * 3421: Debug script invocation
    
    * 3421: Updates working directory
    
    * 3421: Updates script path
    
    * 3421: Removes fetch
    
    * 3421: Updates conditional
    
    * 3421: Adds cd
    
    * 3421: Updates continuation path
    
    * 3421: Changes ~ to $HOME
    
    * 3421: Removes quotes
    
    * 3421: Uses literal
    
    * 3421: Updates parameters
    
    * 3421: Adds parameters to dedicated YAML

---
+++ .circleci/scripts/generate-config.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+echo "TRIGGER_SOURCE: $TRIGGER_SOURCE"
+echo "BUILD_CMS: $BUILD_CMS"
+echo "BUILD_FRONTEND: $BUILD_FRONTEND"
+echo "COPY_DATABASE: $COPY_DATABASE"
+echo "CIRCLE_BRANCH: $CIRCLE_BRANCH"
+
+cd ~/project/.circleci
+
+if [ "$TRIGGER_SOURCE" = "api" ]; then
+  if [ "$BUILD_CMS" = "true" ] && [ "$BUILD_FRONTEND" = "true" ]; then
+    if [ "$COPY_DATABASE" = "true" ]; then
+      yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1) * select(fileIndex == 2) * select(fileIndex == 3)' parameters.yml frontend-config.yml cms-config.yml database-config.yml > generated-config.yml
+    else
+      yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1) * select(fileIndex == 2)' parameters.yml frontend-config.yml cms-config.yml > generated-config.yml
+    fi
+  else
+    if [ "$BUILD_CMS" = "true" ]; then
+      yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' parameters.yml cms-config.yml > generated-config.yml
+    elif [ "$BUILD_FRONTEND" = "true" ]; then
+      yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' parameters.yml frontend-config.yml > generated-config.yml
+    elif [ "$COPY_DATABASE" = "true" ]; then
+      yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' parameters.yml database-config.yml > generated-config.yml
+    else
+      cat <<EOF > generated-config.yml
+version: 2.1
+workflows: {}
+EOF
+    fi
+  fi
+elif [ "$TRIGGER_SOURCE" = "scheduled_pipeline" ]; then
+  cat restart-config.yml > generated-config.yml
+else
+  last_commit=$(git rev-parse "$CIRCLE_BRANCH~1")
+
+  changed_files=$(git diff --name-only "$last_commit" origin/main)
+
+  echo "$changed_files" | grep -q '^cms/'
+  cms_changed=$?
+
+  echo "$changed_files" | grep -q '^frontend/'
+  frontend_changed=$?
+
+  if [ "$cms_changed" -eq 0 ] && [ "$frontend_changed" -eq 0 ]; then
+    yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1) * select(fileIndex == 2)' parameters.yml frontend-config.yml cms-config.yml > generated-config.yml
+  elif [ "$cms_changed" -eq 0 ]; then
+    yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' parameters.yml cms-config.yml > generated-config.yml
+  elif [ "$frontend_changed" -eq 0 ]; then
+    yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' parameters.yml frontend-config.yml > generated-config.yml
+  else
+    cat <<EOF > generated-config.yml
+version: 2.1
+workflows: {}
+EOF
+  fi
+  
+fi
