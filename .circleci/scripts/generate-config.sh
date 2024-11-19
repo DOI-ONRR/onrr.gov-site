3d44cbdd3fcb9d82795de451dc1a99b43a9bbb27
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2024-11-19 12:35:14 -0500

    Refactor CircleCI configuration (#3429)
    * dev: Adds circleci job for restart
    
    * Revert "dev: Adds circleci job for restart"
    
    This reverts commit 6bdf71a5d5386856f32761ec4985490445790c65.
    
    * dev: Loads site feedback conditionally
    
    * dev: fixes when site feedback is renderable.
    
    * dev: Adds vuex
    
    * dev: Updates shared state to set pageLoaded to false before route change
    
    * dev: Adds watch to Page
    
    * dev: Handles tabs route changes
    
    * dev: Logging
    
    * dev: Debugging
    
    * dev: More of the same
    
    * dev: Debug from and to
    
    * dev: Conditionally resets pageLoaded
    
    * dev: Bumps docker image
    
    * dev: Router cleanup
    
    * Refactor CircleCI configuration (#3427)
    
    * Fixes Touchpoints loading (#3420)
    
    * dev: Adds circleci job for restart
    
    * Revert "dev: Adds circleci job for restart"
    
    This reverts commit 6bdf71a5d5386856f32761ec4985490445790c65.
    
    * dev: Loads site feedback conditionally
    
    * dev: fixes when site feedback is renderable.
    
    * dev: Adds vuex
    
    * dev: Updates shared state to set pageLoaded to false before route change
    
    * dev: Adds watch to Page
    
    * dev: Handles tabs route changes
    
    * dev: Logging
    
    * dev: Debugging
    
    * dev: More of the same
    
    * dev: Debug from and to
    
    * dev: Conditionally resets pageLoaded
    
    * dev: Bumps docker image
    
    * dev: Router cleanup
    
    * 3421: Refactors configuration
    
    * 3421: Removes yq install
    
    * 3421: Adds executable perms
    
    * 3421: Debug script invocation
    
    * 3421: Updates working directory
    
    * 3421: Updates script path
    
    * 3421: Removes fetch
    
    * 3421: Updates conditional
    
    * 3421: Adds cd
    
    * 3421: Updates continuation path
    
    * 3421: Changes ~ to $HOME
    
    * 3421: Removes quotes
    
    * 3421: Uses literal
    
    * 3421: Updates parameters
    
    * 3421: Adds parameters to dedicated YAML
    
    * dev: Refactors generate-config.sh
    
    * dev: Fixes merge error

---
+++ .circleci/scripts/generate-config.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+cd ~/project/.circleci
+
+config_files=(parameters.yml)
+
+if [ "$TRIGGER_SOURCE" = "scheduled_pipeline" ]; then
+  cat restart-config.yml > generated-config.yml
+  exit 0
+elif [ "$TRIGGER_SOURCE" = "api" ]; then
+  if [ "$BUILD_CMS" = "true" ]; then
+    config_files+=(cms-config.yml)
+  fi
+  
+  if [ "$BUILD_FRONTEND" = "true" ]; then
+    config_files+=(frontend-config.yml)
+  fi
+  
+  if [ "$COPY_DATABASE" = "true" ]; then
+    config_files+=(database-config.yml)
+  fi
+
+elif [ "$TRIGGER_SOURCE" = "webhook" ]; then
+  last_commit=$(git rev-parse "$CIRCLE_BRANCH~1")
+
+  changed_files=$(git diff --name-only "$last_commit" origin/main)
+
+  echo "$changed_files" | grep -q '^cms/'
+  cms_changed=$?
+
+  echo "$changed_files" | grep -q '^frontend/'
+  frontend_changed=$?
+
+  if [ "$cms_changed" -eq 0 ]; then
+    config_files+=(cms-config.yml)
+  fi
+  
+  if [ "$frontend_changed" -eq 0 ]; then
+    config_files+=(frontend-config.yml)
+  fi
+  
+fi
+
+config_count=${#config_files[@]}
+
+if [ "$config_count" -eq 1 ]; then
+  cat <<EOF > generated-config.yml
+version: 2.1
+workflows: {}
+EOF
+
+  exit 0
+fi
+
+merged="{}"
+
+for file in "${config_files[@]}"; do
+  if [ -f "$file" ]; then
+    merged=$(yq eval-all '. as $item ireduce ({}; . * $item)' <(echo "$merged") "$file")
+  else
+    echo "Warning: $file does not exist. Skipping."
+  fi
+done
+
+echo "$merged" > generated-config.yml
