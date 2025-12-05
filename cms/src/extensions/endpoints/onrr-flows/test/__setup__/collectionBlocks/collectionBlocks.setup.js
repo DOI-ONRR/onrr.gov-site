f7d3357a91d7370c1b18e5a7d99b5c525153d34e
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2025-12-05 16:39:01 -0500

    3862: Adds collection_blocks flow test suite

--- cms/src/extensions/endpoints/onrr-flows/test/__setup__/collectionBlocks/collectionBlocks.setup.js
+++ cms/src/extensions/endpoints/onrr-flows/test/__setup__/collectionBlocks/collectionBlocks.setup.js
@@ -12,23 +12,17 @@ jest.unstable_mockModule('../../../src/operations/collectionBlocks/updateCollect
     updateCollectionBlocksItem: jest.fn()
 }));
 
-jest.unstable_mockModule('../../../src/services/collectionBlocksFlow/collectionBlocks', () => ({
-    runCollectionBlocks: jest.fn()
-}));
-
 import { id, item } from '../../__mocks__/collectionBlocks/collectionBlocks.mocks';
 
 export const getMocks = async () => {
     const { getCollectionBlocksById } = await import('../../../src/operations/collectionBlocks/getCollectionBlocksById');
     const { createCollectionBlocksItem } = await import('../../../src/operations/collectionBlocks/createCollectionBlocksItem');
     const { updateCollectionBlocksItem } = await import('../../../src/operations/collectionBlocks/updateCollectionBlocksItem');
-    const { runCollectionBlocks } = await import('../../../src/services/collectionBlocksFlow/collectionBlocks')
 
     return {
         getCollectionBlocksById,
         createCollectionBlocksItem,
         updateCollectionBlocksItem,
-        runCollectionBlocks,
         id,
         item
     };
