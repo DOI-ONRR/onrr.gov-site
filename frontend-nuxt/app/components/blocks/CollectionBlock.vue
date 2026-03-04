95af749b733f21a8e7db0efda89f6a19b16c2415
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2026-03-04 13:31:12 -0500

    4000: Adds indian gas major portion

--- frontend-nuxt/app/components/blocks/CollectionBlock.vue
+++ frontend-nuxt/app/components/blocks/CollectionBlock.vue
@@ -53,6 +53,7 @@ const items = computed(() => {
   />
   <IndexZones v-else-if="collection === 'index_zones'" />
   <Ibmp v-else-if="collection === 'ibmp'" />
+  <IndianGasMajorPortion v-else-if="collection === 'indian_gas_major_portion'" />
   <template v-else-if="collection === 'announcements'">
     <div v-for="item in items" :key="item.id" class="usa-card margin-bottom-3">
       <div class="usa-card__container">
