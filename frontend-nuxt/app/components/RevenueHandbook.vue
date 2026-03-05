ad5e78dd94d4e573674d47ba9ded154f7e54c58e
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2026-03-05 09:17:09 -0500

    4018: Adds revenue handbook

---
+++ frontend-nuxt/app/components/RevenueHandbook.vue
@@ -0,0 +1,107 @@
+<script setup>
+import getRevenueHandbooks from '@/graphql/queries/collections/pricing/revenue_handbooks/getRevenueHandbooks.gql'
+
+const { data } = await useAsyncQuery(getRevenueHandbooks)
+
+const rows = computed(() =>
+  (data.value?.revenue_handbook ?? []).map((r) => ({
+    id: r.id,
+    chapter: r.chapter,
+    section: r.section,
+    title: r.title,
+    tocPage: r.toc_page,
+    pageUrl: r.url && r.actual_page ? `${r.url}#${r.actual_page}` : null,
+  }))
+)
+
+const searchText = ref('')
+
+const filteredRows = computed(() => {
+  if (!searchText.value) return rows.value
+  const query = searchText.value.toLowerCase()
+  return rows.value.filter((r) => r.title?.toLowerCase().includes(query))
+})
+
+const {
+  currentPage, totalPages, displayedItems: displayedRows,
+  visiblePages, rangeStart, rangeEnd, totalItems, goToPage, resetPage,
+} = usePagination(filteredRows, 10)
+
+watch(searchText, () => {
+  resetPage()
+})
+</script>
+
+<template>
+  <div class="grid-row grid-gap margin-bottom-6">
+    <div class="grid-col-6">
+      <label class="usa-label" for="revenue-handbooks-search">Search</label>
+      <input
+        id="revenue-handbooks-search"
+        v-model="searchText"
+        class="usa-input"
+        type="text"
+        placeholder="Search by title"
+      />
+    </div>
+  </div>
+  <table class="usa-table usa-table--borderless width-full usa-table--fixed">
+    <colgroup>
+      <col style="width: 10%" />
+      <col style="width: 10%" />
+      <col style="width: 65%" />
+      <col style="width: 15%" />
+    </colgroup>
+    <thead>
+      <tr>
+        <th scope="col">Chapter</th>
+        <th scope="col">Section</th>
+        <th scope="col">Title</th>
+        <th scope="col">Page</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="row in displayedRows" :key="row.id">
+        <td>{{ row.chapter }}</td>
+        <td>{{ row.section }}</td>
+        <td>{{ row.title }}</td>
+        <td>
+          <a
+            v-if="row.pageUrl"
+            :href="row.pageUrl"
+            target="_blank"
+            class="usa-link"
+          >
+            {{ row.tocPage }}
+          </a>
+          <span v-else>{{ row.tocPage }}</span>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <PaginationBar
+    class="margin-top-2 margin-bottom-3"
+    :current-page="currentPage"
+    :total-pages="totalPages"
+    :visible-pages="visiblePages"
+    :range-start="rangeStart"
+    :range-end="rangeEnd"
+    :total-items="totalItems"
+    label="records"
+    @page-change="goToPage"
+  />
+</template>
+
+<style lang="scss" scoped>
+@use "uswds-theme" as *;
+
+.usa-table--fixed {
+  table-layout: fixed;
+}
+
+.usa-table tbody tr:nth-child(odd) {
+  th, td {
+    @include u-bg('base-lightest');
+  }
+}
+</style>
--- frontend-nuxt/app/components/blocks/CollectionBlock.vue
+++ frontend-nuxt/app/components/blocks/CollectionBlock.vue
@@ -57,6 +57,7 @@ const items = computed(() => {
   <Nymex v-else-if="collection === 'NYMEX'" />
   <PlantSpecificUcas v-else-if="collection === 'plant_specific_ucas'" />
   <Rulemakings v-else-if="collection === 'rulemakings'" />
+  <RevenueHandbook v-else-if="collection === 'revenue_handbook'" />
   <template v-else-if="collection === 'announcements'">
     <div v-for="item in items" :key="item.id" class="usa-card margin-bottom-3">
       <div class="usa-card__container">
