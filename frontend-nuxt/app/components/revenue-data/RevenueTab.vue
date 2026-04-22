0e6d18a4497c90c532593a0fb1711ead19fb4e8b
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2026-04-21 20:17:11 -0400

    feature/4091: Refactors production and revenue tabs

---
+++ frontend-nuxt/app/components/revenue-data/RevenueTab.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+
+<template>
+  <h3>Revenue</h3>
+  <p>The amount of money collected from natural resources on property owned by the federal government and Native Americans.</p>
+</template>
